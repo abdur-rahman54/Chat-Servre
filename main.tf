24dd5f4f828abafc6ed2ba322f27f2c7d2ed48fd
abdur-rahman54 <abdur.rahman59354@gmail.com>
2025-07-17 02:15:05 +0600

    added terraform files

--- main.tf
+++ main.tf
@@ -4,12 +4,17 @@ provider "aws" {
 	region = "ap-southeast-1"
 	}
 
+data "aws_security_group" "chatserver" {
+	name = "chatserver"  # Name of your existing SG
+}
+
 # Resource block
 resource "aws_instance" "Chat_Server" {
 	ami = "ami-02c7683e4ca3ebf58"
 	instance_type = "t2.micro"
 
 	tags = {
-		Name = "TerraformChatServer"
+		Name = "ChatServer"
 	}
+	key_name = "AwsCli"
 }
