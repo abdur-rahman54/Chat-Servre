b90f70224113cc3caa6b493df046450bd281f193
abdur-rahman54 <abdur.rahman59354@gmail.com>
2025-07-17 02:29:03 +0600

    Edit in gitignore file

--- main.tf
+++ main.tf
@@ -1,20 +1,20 @@
 # Provider Block
 provider "aws" {
-	profile = "default"
-	region = "ap-southeast-1"
-	}
+  profile = "default"
+  region  = "ap-southeast-1"
+}
 
 data "aws_security_group" "chatserver" {
-	name = "chatserver"  # Name of your existing SG
+  name = "chatserver" # Name of your existing SG
 }
 
 # Resource block
 resource "aws_instance" "Chat_Server" {
-	ami = "ami-02c7683e4ca3ebf58"
-	instance_type = "t2.micro"
+  ami           = "ami-02c7683e4ca3ebf58"
+  instance_type = "t2.micro"
 
-	tags = {
-		Name = "ChatServer"
-	}
-	key_name = "AwsCli"
+  tags = {
+    Name = "ChatServer"
+  }
+  key_name = "AwsCli"
 }
--- output.tf
+++ output.tf
@@ -1,4 +1,4 @@
 output "instance_public_ip" {
-	description = "public IP address of the Ec2 instance"
-	value = aws_instance.Chat_Server.public_ip
+  description = "public IP address of the Ec2 instance"
+  value       = aws_instance.Chat_Server.public_ip
 }
